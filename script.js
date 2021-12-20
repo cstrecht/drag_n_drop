9e77e5201b9dd45957d275f7e9dec35f19d52fc8
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-20 12:12:11 +0000

    fixing all the code bugs; build dropping areas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".item").forEach((item) => {
-  item.addEventListener("dragStart", dragStart);
-  item.addEventListener("dragEnd", dragEnd);
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
 });
 
 document.querySelectorAll(".area").forEach((area) => {
@@ -9,6 +9,13 @@ document.querySelectorAll(".area").forEach((area) => {
   area.addEventListener("drop", drop);
 });
 
+document.querySelector(".zone1").addEventListener("dragover", dragOverZoneOne);
+document
+  .querySelector(".zone1")
+  .addEventListener("dragleave", dragLeaveZoneOne);
+document.querySelector(".zone1").addEventListener("drop", dropZoneOne);
+
+// Functions for my items:
 function dragStart(e) {
   e.currentTarget.classList.add("dragging");
 }
@@ -16,13 +23,35 @@ function dragEnd(e) {
   e.currentTarget.classList.remove("dragging");
 }
 
+//Functions Drag&Drop:
 function dragOver(e) {
-  e.preventDefault();
-  e.currentTarget.classList.add("hover");
+  if (e.currentTarget.querySelector(".item") === null) {
+    e.preventDefault(); //prevent default so I can drop the item
+    e.currentTarget.classList.add("hover");
+  }
 }
 function dragLeave(e) {
   e.currentTarget.classList.remove("hover");
 }
+function drop(e) {
+  e.currentTarget.classList.remove("hover");
 
-// only works when dragOver is released
-function drop() {}
+  if (e.currentTarget.querySelector(".item") === null) {
+    let dragItem = document.querySelector(".item.dragging");
+    e.currentTarget.appendChild(dragItem);
+  }
+}
+
+// Functions Zone 1
+function dragOverZoneOne(e) {
+  e.preventDefault();
+  e.currentTarget.classList.add("hover");
+}
+function dragLeaveZoneOne(e) {
+  e.currentTarget.classList.remove("hover");
+}
+function dropZoneOne(e) {
+  e.currentTarget.classList.remove("hover");
+  let dragItem = document.querySelector(".item.dragging");
+  e.currentTarget.appendChild(dragItem);
+}
